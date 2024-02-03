4fcea27febf59a62a2719438c88b7c65e1339966
yijian-physics <102334085+yijian-physics@users.noreply.github.com>
2024-02-02 22:43:18 -0500

    Defect CFT g function

---
+++ DMRG_Fuzzy_Sphere_Defect.jl
@@ -0,0 +1,161 @@
+using ITensors,LinearAlgebra,WignerSymbols
+
+function get_V_from_M(N,i,j,k,l,V0,V1,M0,M1)
+    ## N = 2s+1, get V[i,j,k,l]
+    s = (N-1)/2
+    m1 = -s+i-1
+    m2 = -s+j-1
+    m3 = -s+k-1
+    m4 = -s+l-1
+    if(abs(m1+m2)<=N-1 && abs(m3+m4)<=N-1)
+        tmp1 = V0 * (4*s+1) * M0[i,j] *  M0[l,k]
+    else
+        tmp1 = 0
+    end
+    if(abs(m1+m2)<=N-2 && abs(m3+m4)<=N-2)
+        tmp2 = V1 * (4*s-1) * M1[i,j] *  M1[l,k]
+    else
+        tmp2 = 0
+    end
+    V = tmp1+tmp2
+    return V
+end
+
+function compute_all_3js(N)
+    M0 = zeros(N,N)
+    M1 = zeros(N,N)
+    s = (N-1)/2
+    for i in 1:N
+        for j in 1:N
+            m1 = -s+i-1
+            m2 = -s+j-1
+            if(abs(m1+m2)<=N-1)
+                M0[i,j] = wigner3j(s,s,2*s,m1,m2,-m1-m2)
+            end
+            if(abs(m1+m2)<=N-2)
+                M1[i,j] = wigner3j(s,s,2*s-1,m1,m2,-m1-m2)
+            end
+        end
+    end
+    return M0,M1
+end
+
+function ITensors.space(
+  ::SiteType"Electron";
+  Lz::Int = 1,
+  conserve_sz=false,
+  conserve_nf=true,
+  conserve_lz=true,
+  conserve_spin_parity=true,
+  qnname_sz="Sz",
+  qnname_nf="Nf",
+  qnname_lz="Lz",
+  qnname_spin_parity="Z2"
+)
+  ## Note that conserve nf and lz is always set on. Lz is TWICE the actual angular momentum
+  if ((!conserve_sz) && conserve_spin_parity)
+    return [
+      QN((qnname_nf, 0, -1), (qnname_lz, 0), (qnname_spin_parity, 0, 2)) => 1
+      QN((qnname_nf, 1, -1), (qnname_lz, Lz),(qnname_spin_parity, 1, 2)) => 1
+      QN((qnname_nf, 1, -1), (qnname_lz, Lz),(qnname_spin_parity, 0, 2)) => 1
+      QN((qnname_nf, 2, -1), (qnname_lz, 2*Lz),(qnname_spin_parity, 1, 2)) => 1
+    ]
+  elseif (!conserve_sz)
+    return [
+      QN((qnname_nf, 0, -1), (qnname_lz, 0)) => 1
+      QN((qnname_nf, 1, -1), (qnname_lz, Lz)) => 1
+      QN((qnname_nf, 1, -1), (qnname_lz, Lz)) => 1
+      QN((qnname_nf, 2, -1), (qnname_lz, 2*Lz)) => 1
+    ]        
+  end
+  return 4
+end
+
+mutable struct myObserver <: AbstractObserver
+   energy_tol::Float64
+   last_energy::Float64
+
+   myObserver(energy_tol=0.0) = new(energy_tol,1000.0)
+end
+
+function ITensors.checkdone!(o::myObserver;kwargs...)
+  sw = kwargs[:sweep]
+  energy = kwargs[:energy]
+  if abs(energy-o.last_energy)/abs(energy) < o.energy_tol
+    println("Stopping DMRG after sweep $sw")
+    return true
+  end
+  # Otherwise, update last_energy and keep going
+  o.last_energy = energy
+  return false
+end
+
+function DMRG_Fuzzy_Sphere_Ising_defect(N::Int,hN,hS;V0=4.75,V1=1.0,h=3.16,hz=1000.0,max_bond_dim=400,nsweeps=8,epsilon=1E-10,etol=1E-12)
+    ## ZZ+X convention, does not conserve Z2
+    ## hN, hS = "+","0","-"
+    M0,M1 = compute_all_3js(N)
+    sites = [siteind("Electron",Lz=2*i-N-1, conserve_sz=false,conserve_nf=true,conserve_lz=true,conserve_spin_parity=false) for i in 1:N]
+    os = OpSum()
+    for i in 1:N
+        for j in 1:N
+            for k in 1:N
+                for l in 1:N
+                    if(i+j == k+l)
+                        V = get_V_from_M(N,i,j,k,l,V0,V1,M0,M1)
+                        os += 2*V,"Cdagup",i,"Cup",l,"Cdagdn",j,"Cdn",k
+                    end
+                end
+            end
+        end
+    end
+    
+    for i in 1:N
+        os += -2*h, "Sx", i
+    end
+    
+    if(hN=="+")
+        os += -hz, "Sz", 1
+    elseif(hN=="-")
+        os += hz, "Sz", 1
+    end
+    
+    if(hS=="+")
+        os += -hz, "Sz", N
+    elseif(hS=="-")
+        os += hz, "Sz", N
+    end
+    
+    H = MPO(os,sites,splitblocks=false)
+    
+    maxdim = [10,20,100,200,max_bond_dim] 
+    noise = [1E-4,1E-6,1E-8,0,0] #add some noise
+    cutoff = [epsilon] 
+    
+    state = ["Up" for n=1:N]
+    if(hN=="-")
+        state[1] = "Dn"
+    end
+    if(hS=="-")
+        state[N] = "Dn"
+    end
+    
+    psi_prod = MPS(sites,state)
+   
+    obs = myObserver(etol)
+    
+    E0,psi0 = dmrg(H,psi_prod; nsweeps, maxdim, cutoff,noise,observer=obs);  # ground state 
+    return E0,psi0
+end
+
+
+function compute_g(N::Int;max_bd=2000,max_sweep=15)
+    ~,psi00 = DMRG_Fuzzy_Sphere_Ising_defect(N,0,0,max_bond_dim = max_bd,nsweeps=max_sweep);
+    ~,psi10 = DMRG_Fuzzy_Sphere_Ising_defect(N,"+",0,max_bond_dim = max_bd,nsweeps=max_sweep);
+    ~,psi11 = DMRG_Fuzzy_Sphere_Ising_defect(N,"+","+",max_bond_dim = max_bd,nsweeps=max_sweep);
+    g = (inner(psi00',psi10)/inner(psi10',psi11))^2
+    return g
+end
+
+N = 12
+g = compute_g(N)
+@show g
